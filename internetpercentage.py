d876bfa6c04b854258902427f48af3bce922477f
Rmcavin <rmcavin@gmail.com>
2016-10-30 22:03:00 -0500

    Header Information
    A program to calculate percentage of  a country population using the
    internet.

---
+++ internetpercentage.py
@@ -0,0 +1,9 @@
+##Purpose: This script processes two data sets from the CIA World Factbook. Two
+##independent text files, one for population and one for internet users, are
+##merged by country, then a fourth field is added for percentage internet users,
+##and finally the percentage of the polulation that uses the internet is
+##calculated and input into the new field. This data serves as the basis for my
+##leaflet map.
+##
+##Author: Rachel Cavin
+##Date: October 30th, 2016
