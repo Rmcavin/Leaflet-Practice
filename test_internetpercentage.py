c12b7a5e3b19f72b01a23f1c90ff0ccfb10b830b
Rmcavin <rmcavin@gmail.com>
2016-11-02 23:06:53 -0500

    Defined class, test for getdata function

--- test_internetpercentage.py
+++ test_internetpercentage.py
@@ -5,5 +5,15 @@
 import unittest
 from internetpercentage import getdata, getpercentage, findGaps
 
+class TestInternetPercentage(unittest.TestCase):
+
+    def test_get_data(self):
+        self.list1 = ['1      China      626600000']
+        self.list2 = ['1      China       1373541278']
+        self.dictionary = {}
+        getdata(self.list1, self.dictionary)
+        getdata(self.list2, self.dictionary)
+        self.assertEqual(self.dictionary, {'China': ['626600000','1373541278']})
+
 if __name__ == '__main__':
     unittest.main()
